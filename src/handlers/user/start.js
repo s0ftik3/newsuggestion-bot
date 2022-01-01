6340b0a5df334be0a734296bafa97fb0f52dcb83
Vyacheslav <slava021323@gmail.com>
2022-01-01 18:11:08 +0600

    Gosh goodbye, it's the last time

--- src/handlers/user/start.js
+++ src/handlers/user/start.js
@@ -1,63 +1,48 @@
-const Markup = require('telegraf/markup');
-const getUser = require('../../database/getUser');
-const recordUser = require('../../database/recordUser');
-const replyWithError = require('../../scripts/replyWithError');
+const Markup = require("telegraf/markup");
+const getUser = require("../../database/getUser");
+const recordUser = require("../../database/recordUser");
+const replyWithError = require("../../scripts/replyWithError");
 
 module.exports = () => async (ctx) => {
     try {
-        if (ctx.chat.type !== 'private') return;
+        if (ctx.chat.type !== "private") return;
         const user = await getUser(ctx.from.id);
 
         if (user === null) {
             const data = {
                 id: ctx.from.id,
-                firstName: ctx.from.first_name == undefined ? null : ctx.from.first_name,
-                lastName: ctx.from.last_name == undefined ? null : ctx.from.last_name,
-                username: ctx.from.username == undefined ? null : ctx.from.username,
+                firstName:
+                    ctx.from.first_name == undefined
+                        ? null
+                        : ctx.from.first_name,
+                lastName:
+                    ctx.from.last_name == undefined ? null : ctx.from.last_name,
+                username:
+                    ctx.from.username == undefined ? null : ctx.from.username,
                 timestamp: new Date(),
             };
 
             recordUser(data).then(() => {
-                ctx.replyWithMarkdown(ctx.i18n.t('service.greeting', { name: ctx.from.first_name }), {
-                    reply_markup: Markup.inlineKeyboard(
-                        [
-                            Markup.callbackButton(ctx.i18n.t('button.sFeature'), 'sFeature'),
-                            Markup.callbackButton(ctx.i18n.t('button.sLanguage'), 'sLanguage'),
-                            Markup.callbackButton(ctx.i18n.t('button.settings'), 'settings'),
-                        ],
-                        { columns: 1 }
-                    ),
-                });
+                ctx.replyWithMarkdown(ctx.i18n.t("service.greeting"));
             });
         } else {
             ctx.session.user = user;
             ctx.i18n.locale(ctx.session.user.language);
 
-            if (ctx.updateType === 'callback_query') {
-                ctx.editMessageText(ctx.i18n.t('service.greeting', { name: ctx.from.first_name }), {
-                    parse_mode: 'Markdown',
-                    reply_markup: Markup.inlineKeyboard(
-                        [
-                            Markup.callbackButton(ctx.i18n.t('button.sFeature'), 'sFeature'),
-                            Markup.callbackButton(ctx.i18n.t('button.sLanguage'), 'sLanguage'),
-                            Markup.callbackButton(ctx.i18n.t('button.settings'), 'settings'),
-                        ],
-                        { columns: 1 }
-                    ),
-                });
+            if (ctx.updateType === "callback_query") {
+                ctx.editMessageText(
+                    ctx.i18n.t("service.greeting", {
+                        name: ctx.from.first_name,
+                    }),
+                );
 
                 ctx.answerCbQuery();
             } else {
-                ctx.replyWithMarkdown(ctx.i18n.t('service.greeting', { name: ctx.from.first_name }), {
-                    reply_markup: Markup.inlineKeyboard(
-                        [
-                            Markup.callbackButton(ctx.i18n.t('button.sFeature'), 'sFeature'),
-                            Markup.callbackButton(ctx.i18n.t('button.sLanguage'), 'sLanguage'),
-                            Markup.callbackButton(ctx.i18n.t('button.settings'), 'settings'),
-                        ],
-                        { columns: 1 }
-                    ),
-                });
+                ctx.replyWithMarkdown(
+                    ctx.i18n.t("service.greeting", {
+                        name: ctx.from.first_name,
+                    }),
+                );
             }
         }
     } catch (err) {
